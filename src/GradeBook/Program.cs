be232c4d824df710bf66ece135ad796068606ee3
chewy927 <jeremyoutinen@gmail.com>
2021-04-16 15:21:32 -0500

    Find the highest and lowest grades then output
    them to the console with the average.

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -9,22 +9,31 @@ namespace GradeBook
         {
             var book = new Book("Jeremy's Grade Book");
             book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(77.5);
 
             var grades = new List<double>() {12.7, 34.7, 12.4, 6.5};
             grades.Add(56.1);
 
             // Calculate the sum of all elements in the grades list
             var result = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
             foreach(var number in grades)
             {
+                highGrade = Math.Max(number, highGrade);
+                lowGrade = Math.Min(number, lowGrade); 
                 result += number;
             }
-            Console.WriteLine($"The sum of scores is: {result:N1}");
 
             // Compute the average
             var average = result/grades.Count;
+            // Write the average to the console
             System.Console.WriteLine($"The average score is: {average:N1}");
-
+            // Write the highest grade to the console
+            System.Console.WriteLine($"The highest score is: {highGrade:N1}");
+            // Write the lowest grade to the console
+            System.Console.WriteLine($"The lowest score is: {lowGrade:N1}");
         }
     }
 }
