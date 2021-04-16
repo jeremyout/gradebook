d5b0d3c5244b3679043d3f61769a3682b47ad0cb
chewy927 <jeremyoutinen@gmail.com>
2021-04-16 15:45:00 -0500

    Split all the calculations into their own methods,
    then perform all of them in the ComputeStatistics
    method, then print them to the console in the
    ShowStatistics method.

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -1,4 +1,4 @@
-
+using System;
 using System.Collections.Generic;
 
 namespace GradeBook
@@ -17,7 +17,56 @@ namespace GradeBook
             grades.Add(grade);
         }
 
+        public void FindHighGrade()
+        {
+            foreach(var number in grades)
+            {
+                highGrade = Math.Max(number, highGrade);
+            }
+        }
+
+        public void FindLowGrade()
+        {
+            foreach(var number in grades)
+            {
+                lowGrade = Math.Min(number, lowGrade);
+            }
+        }
+
+        public void FindAverageGrade()
+        {
+            var result = 0.0;
+            foreach(var number in grades)
+            {
+                result += number;
+            }
+            average = result/grades.Count;
+        }
+
+        private void ComputeStatistics()
+        {
+            FindHighGrade();
+            FindLowGrade();
+            FindAverageGrade();
+        }
+
+        public void ShowStatistics()
+        {   
+            // Run the calculations
+            ComputeStatistics();
+
+            // Write the highest grade to the console
+            System.Console.WriteLine($"The highest score is: {highGrade:N1}");
+            // Write the lowest grade to the console
+            System.Console.WriteLine($"The lowest score is: {lowGrade:N1}");
+            // Write the average to the console
+            System.Console.WriteLine($"The average score is: {average:N1}");
+        }
+
         private List<double> grades;
         private string name;
+        private double average = 0.0;
+        private double highGrade = double.MinValue;
+        private double lowGrade = double.MaxValue;
     }
 }
