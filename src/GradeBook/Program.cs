6d627114d7065ceb934280fa5688aece8f196f71
chewy927 <jeremyoutinen@gmail.com>
2021-04-26 21:51:35 -0500

    Challenge: Remove hardcoded grades and have
    the user input grades, typing Q will quit grade input and
    then output the statistics.

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -8,10 +8,23 @@ namespace GradeBook
         static void Main(string[] args)
         {
             var book = new Book("Jeremy's Grade Book");
-            book.AddGrade(89.1);
-            book.AddGrade(90.5);
-            book.AddGrade(77.5);
-            book.AddGrade(12.5);
+            var grade = 0.0;
+            var done = false;
+            while (!done)
+            {
+                Console.WriteLine("Enter a grade or enter 'q' to quit:");
+                var input = Console.ReadLine();
+                if (input.Equals("q") || input.Equals("Q"))
+                {
+                    done = true;
+                }
+                else
+                {
+                    grade = double.Parse(input);
+                    book.AddGrade(grade);
+                }
+
+            }
             book.ShowStatistics();
         }
     }
