1812318dccc8f1c36fad02ab031854bd6576e57e
chewy927 <jeremyoutinen@gmail.com>
2021-04-28 15:04:27 -0500

    Add new delegate demo test

--- test/GradeBook.Tests/TypeTests.cs
+++ test/GradeBook.Tests/TypeTests.cs
@@ -3,8 +3,27 @@ using Xunit;
 
 namespace GradeBook.Tests
 {
+
+    public delegate string WriteLogDelegate(string logMessage);
+
     public class TypeTests
     {
+        [Fact]
+        public void WriteLogDelegateCanPointToMethod()
+        {
+            WriteLogDelegate log;
+            
+            log = new WriteLogDelegate(ReturnMessage);
+
+            var result = log("Hello");
+            Assert.Equal("Hello", result);
+        }
+
+        string ReturnMessage(string message)
+        {
+            return message;
+        }
+
         [Fact]
         public void StringsBehaveLikeValueTypes()
         {
